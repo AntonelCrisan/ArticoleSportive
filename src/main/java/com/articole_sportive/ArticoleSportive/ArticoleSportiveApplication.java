6bf2552bdb27283fcc27064bde6feddb8ff6495a
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2024-11-04 10:19:15 +0200

    Adaugare MOSCOW in readme.md

---
+++ src/main/java/com/articole_sportive/ArticoleSportive/ArticoleSportiveApplication.java
@@ -0,0 +1,13 @@
+package com.articole_sportive.ArticoleSportive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticoleSportiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticoleSportiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/articole_sportive/ArticoleSportive/ArticoleSportiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articole_sportive.ArticoleSportive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticoleSportiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
